d28782c92e23f32bb6fe95ab50b6182f61672fe1
kumarkundan862 <kumarkundan862@gmail.com>
2021-02-11 22:02:22 +0530

    added functionality in common.js file

---
+++ scripts/common.js
@@ -0,0 +1,22 @@
+// Get the modal
+var signUpModal = document.getElementById("signUpModal");
+var signInModal = document.getElementById("signInModal");
+
+// Get the button that opens the modal
+var signUpBtn = document.getElementById("SignUp");
+var signInBtn = document.getElementById("SignIn");
+
+// When the user clicks on the button, open the modal
+signUpBtn.onclick = function() {
+  signUpModal.style.display = "block";
+}
+
+// When the user clicks on the button, open the modal
+signInBtn.onclick = function() {
+  signInModal.style.display = "block";
+}
+
+
+
+
+
